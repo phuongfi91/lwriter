8c54ffde70623dead8bd49d477e5d6c061a00ace
Phuong Nguyen <phuong.fi.91@gmail.com>
2018-10-06 11:20:10 +0300

    Implement separate scrollbar for each translation window

--- src/App/App.js
+++ src/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import GoogleTranslate from './GoogleTranslate/GoogleTranslate';
 import MediaWikiLoader from './MediaWikiLoader/MediaWikiLoader';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 class App extends Component {
 
@@ -72,19 +73,23 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <GoogleTranslate
-          onSelectionChange={(newSelection) => this.handleSelectionChange(newSelection)}
-          currentHistoryIndex={this.state.currentHistoryIndex}
-          historyLength={this.state.history.length}
-          onClickBack={event => this.goBack(event)}
-          onClickForward={event => this.goForward(event)}
-        />
-        <MediaWikiLoader
-          lookup={this.state.lookup}
-          onGoToAnotherArticle={anotherArticleName => this.handleSelectionChange(anotherArticleName)}
-        />
-      </div>
+      <Scrollbars
+        autoHide
+        style={{ width: '100%', height: 'calc(100vh - 60px)' }}>
+        <div className="App">
+          <GoogleTranslate
+            onSelectionChange={(newSelection) => this.handleSelectionChange(newSelection)}
+            currentHistoryIndex={this.state.currentHistoryIndex}
+            historyLength={this.state.history.length}
+            onClickBack={event => this.goBack(event)}
+            onClickForward={event => this.goForward(event)}
+          />
+          <MediaWikiLoader
+            lookup={this.state.lookup}
+            onGoToAnotherArticle={anotherArticleName => this.handleSelectionChange(anotherArticleName)}
+          />
+        </div>
+      </Scrollbars>
     );
   }
 }
